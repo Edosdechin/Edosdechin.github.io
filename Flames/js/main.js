b2173efec1ccd1d5c9c521b047a23223d5606bc4
Francisco Alberto <33960594+Edosdechin@users.noreply.github.com>
2022-06-02 09:43:35 -0700

    Add files via upload

---
+++ Flames/js/main.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector('#open-menu-btn')
+const closeMenu = document.querySelector('#close-menu-btn')
+const menu = document.querySelector('.nav_menu')
+
+
+//open nav menu
+
+menuBtn.addEventListener('click', () =>{
+	menu.style.display = 'block'
+	closeMenu.style.display = 'inline-block'
+	menuBtn.style.display = 'none'
+})
+
+closeMenu.addEventListener('click', () =>{
+	menu.style.display = 'none'
+	closeMenu.style.display = 'none'
+	menuBtn.style.display = 'inline-block'
+})
